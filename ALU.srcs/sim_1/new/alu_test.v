5ba8139df33cbf0b1cc112ad3845b5f666f763fd
nicolas <nicolasdelamata@mi.unc.edu.ar>
2026-08-31 19:06:01 -0300

    Implementacion del testbench para la funcion add de la alu

---
+++ ALU.srcs/sim_1/new/alu_test.v
@@ -0,0 +1,95 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 08/31/2026 04:55:52 PM
+// Design Name: 
+// Module Name: alu_test
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module tb_alu_top;
+    
+    localparam NB_DATA = 8;
+    localparam NB_OP = 6;
+    
+    reg [NB_DATA-1:0] i_switches;
+    reg i_load_a;
+    reg i_load_b;
+    reg i_load_c;
+    reg i_clk;
+    integer i = 0;
+    wire [NB_DATA-1:0] o_leds;
+    
+    alu_top #(
+        .NB_DATA(NB_DATA),
+        .NB_OP(NB_OP)
+    ) u_01(
+        .i_switches(i_switches),
+        .i_load_a(i_load_a),
+        .i_load_b(i_load_b),
+        .i_load_c(i_load_c),
+        .i_clk(i_clk),
+        .o_leds(o_leds)
+    );
+    
+    initial
+    begin
+        #0
+        i_clk = 1'b1;
+        i_switches = {NB_DATA{1'b0}};
+        i_load_a = 1'b0;
+        i_load_b = 1'b0;
+        i_load_c = 1'b0;
+        @(negedge i_clk);
+        i_switches = 8'd10;
+        i_load_a = 1'b1;
+        @(posedge i_clk);
+        #1;
+        $display("r_a = %d", u_01.r_a); 
+        @(negedge i_clk);
+        i_load_a = 0;
+        i_load_b = 1;
+        i_switches = 8'd5;
+        @(posedge i_clk);
+        #1;
+        $display("r_b = %d", u_01.r_b);
+        @(negedge i_clk);
+        i_load_b = 0;
+        i_switches = 6'b100000;
+        i_load_c = 1;
+        @(posedge i_clk);
+        #1;
+        $display("r_op = %b", u_01.r_op);
+        #1;
+        $display("o_leds = %d", u_01.o_leds);
+        #250 $finish;
+    end
+    
+/*    initial
+    begin
+        #201
+        $display("Clock cambio %d", i);
+        #250 $finish;
+    end
+*/    
+    always
+    begin
+        #5
+        i_clk = ~i_clk;
+        //i = i + 1;
+    end
+   
+endmodule
