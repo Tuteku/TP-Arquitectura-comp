17dc776c1a1994893ca9d1bcac6928538cb24f07
Mateo <mateo.quispe@mi.unc.edu.ar>
2026-09-01 18:16:25 -0300

    separar archivos tb

---
+++ ALU.srcs/sim_1/new/ALU_test.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 08/31/2026 04:54:34 PM
+// Design Name: 
+// Module Name: ALU_test
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module ALU_test(
+
+    );
+endmodule
---
+++ ALU.srcs/sim_1/new/tb_alu.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 09/01/2026 06:01:02 PM
+// Design Name: 
+// Module Name: tb_alu
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module tb_alu(
+
+    );
+endmodule
